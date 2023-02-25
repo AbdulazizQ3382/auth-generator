f85fc8e8bffdb57d6186e49292999b3ae9203a8e
abdulazizQ3382 <abuqannam2@gmail.com>
2023-02-25 22:50:35 +0300

    project init

---
+++ src/main/java/com/auth/generator/authgenerator/AuthGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.auth.generator.authgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthGeneratorApplication.class, args);
+    }
+
+}
